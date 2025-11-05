879b61726e4e19284391ba563853c7ec0ea3e42c
Valerka.ya <lera.chazova2012@yandex.ru>
2025-11-05 19:15:48 +0200

    add server client and middleware

---
+++ middleware.ts
@@ -0,0 +1,13 @@
+// /middleware.ts (корень)
+import type { NextRequest } from "next/server";
+import { updateSession } from "@/lib/supabase/middleware";
+
+export async function middleware(req: NextRequest) {
+  return updateSession(req);
+}
+
+export const config = {
+  matcher: [
+    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+  ],
+};
