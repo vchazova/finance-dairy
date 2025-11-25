65f6726bb3fbdf46a3843016cea8ca4f8665abd9
Valerka.ya <lera.chazova2012@yandex.ru>
2025-11-25 16:53:08 +0200

    refactor: remove unused categoryLabelMap dependency from useEffect

--- src/app/[workspace_id]/page.client.tsx
+++ src/app/[workspace_id]/page.client.tsx
@@ -146,7 +146,6 @@ export default function WorkspaceClientPage({
     initialCategories.length,
     initialPaymentTypes.length,
     initialCurrencies.length,
-    categoryLabelMap,
   ]);
 
   return (
